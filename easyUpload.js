c2ed6a9b780290f3eef0fd9a1b54f111ef8d4e05
QueQingyun <qingyun.que@ele.me>
2018-01-23 10:00:04 +0800

    delete character templates

--- easyUpload.js
+++ easyUpload.js
@@ -399,7 +399,7 @@ https://github.com/funnyque
         },
         _findEle: function(index, target) {
           var obj = {};
-          obj.ele = $(target).find(`.easy_upload_queue_item[data-index=${index}]`);
+          obj.ele = $(target).find('.easy_upload_queue_item[data-index=' + index +']');
           obj.upBar = $(obj.ele).find('.easy_upload_bar');
           obj.upPeacent = $(obj.ele).find('.easy_upload_percent');
           obj.statusDiv = $(obj.ele).find('.easy_upload_status');
@@ -435,7 +435,7 @@ https://github.com/funnyque
             }); 
           }
           arr.forEach(function(item){
-            $(target).find(`.easy_upload_queue_item[data-index=${item}]`).hide().find('.queue_check').hide();
+            $(target).find('.easy_upload_queue_item[data-index=' + item +']').hide().find('.queue_check').hide();
             if (option.multi) dele(item);
             var qItem = _this._findEle(item, target);
             if (qItem.upStatus=='2') deleAllowFiles(item);
